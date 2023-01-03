1c481d0a2d589fb0fb7532ea5a47084ea0944388
Dao Duong <dao.duong@saritasa.com>
2023-01-03 17:15:35 +0700

    Add model and post method JC19-506

--- index.js
+++ index.js
@@ -2,17 +2,34 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import connectDatabase from './config/db.config.js';
+import Post from './model/post.js';
 
 const app = express();
+app.use(express.json());
 
 const requestListener = (req, res) => {
     res.sendFile(path.join(path.dirname(fileURLToPath(import.meta.url)), 'index.html'));
 }
 
+
 // Connect database
 connectDatabase();
 
-app.get('/', requestListener);
+app.post('/', (req, res) => {
+    const post = new Post(req.body)
+    post.save()
+        .then(() => {
+            res.send('Post saved successfully');
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
+
+app.get('/', async (req, res) => {
+    const post = await Post.find();
+    res.send(post);
+});
 
 const PORT = process.env.PORT || 3000;
 
