32b021c4967a99fdab717cab8b8f64718844b751
Dao Duong <dao.duong@saritasa.com>
2023-01-04 17:05:48 +0700

    Remove database connection JC19-506

--- ex1/index.js
+++ ex1/index.js
@@ -1,28 +1,7 @@
 import express from 'express';
-import connectDatabase from './config/db.config.js';
-import Post from './model/post.js';
 
 const app = express();
 app.use(express.json());
-
-// Connect database
-// connectDatabase();
-
-app.post('/posts', (req, res) => {
-    const post = new Post(req.body)
-    post.save()
-        .then(() => {
-            res.send('Post saved successfully');
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-})
-
-app.get('/posts', async (req, res) => {
-    const post = await Post.find();
-    res.send(post);
-});
 
 app.get('/', (req, res) => {
     res.send('Hello World');
