2f50a14f8aa58d19eecc93ea2254831974aa49b2
Dao Duong <dao.duong@saritasa.com>
2023-01-10 16:57:39 +0700

    Change to node base request handler JC19-506

--- ex1/index.js
+++ ex1/index.js
@@ -1,15 +1,38 @@
-import express from 'express';
-
-const app = express();
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
+import * as http from 'http';
+import { readFileSync } from 'fs';
+import routes from './routes.js';
 
 const PORT = process.env.PORT || 3000;
 
+const requestHtmlListener = (req, res) => {
+    res.setHeader('Content-Type', 'text/html')
+    res.writeHead(200);
+    res.end(readFileSync('./index.html'));
+}
 
-app.listen(PORT, () => {
+const requestListener = (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    switch(req.url) {
+        case '/':
+            requestHtmlListener(req, res);
+            break;
+        case '/books':
+            res.writeHead(200);
+            res.end(routes.books);
+            break;
+        case '/authors':
+            res.writeHead(200);
+            res.end(routes.authors);
+            break;
+        default:
+            res.writeHead(404);
+            res.end(JSON.stringify({error:"Resource not found"}));
+            break;
+    }
+}
+
+const server = http.createServer(requestListener);
+
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
+})
