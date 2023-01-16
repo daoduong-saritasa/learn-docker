21eb509d27f9308475c886024f7677b0892a66e0
Dao Duong <dao.duong@saritasa.com>
2023-01-16 12:02:45 +0700

    Correct endpoint JC19-509

--- ex2/api/routes.js
+++ ex2/api/routes.js
@@ -1,9 +1,9 @@
 const routes = {
     auth: {
-        login: '/auth/login',
+        login: '/auth/login/',
     },
     user: {
-        profile: '/user/profile'
+        profile: '/users/profile/'
     }
 }
 
