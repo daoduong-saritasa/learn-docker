aa15b55626288ffcee69677e6a55e3fe807e1974
DaoDuong <dao-duong@saritasa.com>
2023-02-11 16:36:20 +0700

    Add client JC19-511

---
+++ ex3/client/projects/common/src/lib/core/guards/unauthorized.guard.ts
@@ -0,0 +1,17 @@
+import { inject } from '@angular/core';
+import { Router, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { first, map } from 'rxjs/operators';
+
+import { UserService } from '../services/user.service';
+
+/** Guard prevents user from accessing if a user is not logged in. */
+export function unauthorizedGuard(): Observable<boolean | UrlTree> {
+  const userService = inject(UserService);
+  const router = inject(Router);
+
+  return userService.isAuthorized$.pipe(
+    map(isAuthorized => (isAuthorized ? true : router.parseUrl('/'))),
+    first(),
+  );
+}
---
+++ ex3/client/projects/common/src/lib/shared/components/label/label.component.spec.ts
@@ -0,0 +1,50 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { byTestId, createHostFactory, SpectatorHost } from '@ngneat/spectator';
+
+import { FormErrorWrapperComponent } from '../form-error-wrapper/form-error-wrapper.component';
+
+import { ValidationMessageComponent } from '../validation-message/validation-message.component';
+
+import { LabelComponent } from './label.component';
+
+describe('LabelComponent', () => {
+  let spectator: SpectatorHost<LabelComponent>;
+
+  const createSpectator = createHostFactory({
+    component: LabelComponent,
+    declarations: [ValidationMessageComponent, FormErrorWrapperComponent],
+    imports: [ReactiveFormsModule, FormsModule],
+  });
+
+  describe('label text', () => {
+    describe('is specified', () => {
+      it('renders it', () => {
+        const labelText = 'Test label';
+
+        spectator = createSpectator(`<saanboc-label labelText="${labelText}"/>`);
+
+        expect(spectator.query(byTestId('label'))).toHaveText(labelText);
+      });
+    });
+
+    describe('is not specified', () => {
+      it('does not render it', () => {
+        spectator = createSpectator(`<saanboc-label/>`);
+
+        expect(spectator.query(byTestId('label'))).toBeNull();
+      });
+    });
+  });
+
+  describe('content', () => {
+    const contentText = 'Hello';
+
+    beforeEach(() => {
+      spectator = createSpectator(`<saanboc-label><span data-testid="message">${contentText}</span></saanboc-label>`);
+    });
+
+    it('is rendered', () => {
+      expect(spectator.query(byTestId('message'))).toHaveText(contentText);
+    });
+  });
+});
